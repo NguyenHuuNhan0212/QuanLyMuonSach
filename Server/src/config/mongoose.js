f65254b03ab41fea4012f929bc09cd9ce981bc54
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-05-24 17:00:41 +0700

    update

--- Server/src/config/mongoose.js
+++ Server/src/config/mongoose.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose')
 
 const connect = async (url) => {
-    try{
-        await  mongoose.connect(url)
+    try {
+        await mongoose.connect(url)
         console.log("Kết nối cơ sở dữ liệu thành công")
-    }catch(err) {
+    } catch (err) {
         console.log("Kết nối cơ sở dữ liệu thất bại")
-        process.exit(1)// Dừng ngay lập tức
+        process.exit(1) // Dừng ngay lập tức
     }
-   
 }
 module.exports = connect
