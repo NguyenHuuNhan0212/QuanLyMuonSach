e0e9ac90da9a7ebb15e5a18ca2e2618550690e58
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-06-14 15:14:49 +0700

    finished

--- Client/src/axios/api.service.js
+++ Client/src/axios/api.service.js
@@ -1,15 +1,7 @@
-// import axios from 'axios';
-
-// export const axiosInstance = axios.create({
-//   baseURL: 'http://localhost:3000/',
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-// });
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,  // ✅ lấy từ biến môi trường
+  baseURL: 'http://localhost:3000/',
   headers: {
     'Content-Type': 'application/json',
   },
