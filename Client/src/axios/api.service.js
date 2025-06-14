5ce75c16f11855d92765887fd21e9619759fb537
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-06-14 15:11:18 +0700

    use VITE_API_URL instead of localhost

--- Client/src/axios/api.service.js
+++ Client/src/axios/api.service.js
@@ -1,7 +1,15 @@
+// import axios from 'axios';
+
+// export const axiosInstance = axios.create({
+//   baseURL: 'http://localhost:3000/',
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+// });
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/',
+  baseURL: import.meta.env.VITE_API_URL,  // ✅ lấy từ biến môi trường
   headers: {
     'Content-Type': 'application/json',
   },
