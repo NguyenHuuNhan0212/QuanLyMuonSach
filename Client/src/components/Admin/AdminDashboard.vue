5dc40201e3ffe0618fb16e9f7eff3a60d2498bbd
NguyenHuuNhan0212 <nhanvigo@gmail.com>
2025-08-07 12:47:17 +0700

    update route dashboard admin

--- Client/src/components/Admin/AdminDashboard.vue
+++ Client/src/components/Admin/AdminDashboard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container my-4" data-aos="fade-up" data-aos-duration="1000">
-    <h4 class="mb-4 text-center" >Dashboard</h4>
+    <h4 class="mb-4 text-center">Dashboard</h4>
     <div class="row g-3">
       <div class="col-md-4">
         <div class="card text-white text-center" :style="{ backgroundColor: '#17a2b8' }">
@@ -14,13 +14,14 @@
             </div>
           </div>
           <div class="card-footer bg-transparent border-top-0">
-            <router-link class="nav-link" :to="{name: 'quanlysach'}"><small>Xem thêm <i class="fas fa-arrow-circle-right"></i></small></router-link>
+            <router-link class="nav-link" :to="{ name: 'quanlysach' }"><small>Xem thêm <i
+                  class="fas fa-arrow-circle-right"></i></small></router-link>
           </div>
         </div>
       </div>
 
       <div class="col-md-4">
-        <div class="card text-white text-center" :style="{backgroundColor: '#28a745' }">
+        <div class="card text-white text-center" :style="{ backgroundColor: '#28a745' }">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-center">
               <div>
@@ -31,30 +32,32 @@
             </div>
           </div>
           <div class="card-footer bg-transparent border-top-0">
-            <router-link class="nav-link" :to="{name: 'quanlynxb'}"> <small>Xem thêm <i class="fas fa-arrow-circle-right"></i></small></router-link>
-           
+            <router-link class="nav-link" :to="{ name: 'quanlynxb' }"> <small>Xem thêm <i
+                  class="fas fa-arrow-circle-right"></i></small></router-link>
+
           </div>
         </div>
       </div>
 
       <div class="col-md-4">
-        <div class="card text-white text-center" :style="{backgroundColor: '#fd7e14' }">
+        <div class="card text-white text-center" :style="{ backgroundColor: '#fd7e14' }">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-center">
               <div>
                 <h2 class="fw-bold">{{ borrowCount || '0' }}</h2>
                 <p class="mb-1">Tổng số phiếu mượn</p>
               </div>
-            <i class="fa-solid fa-file-pen big-icon"></i>
+              <i class="fa-solid fa-file-pen big-icon"></i>
             </div>
           </div>
           <div class="card-footer bg-transparent border-top-0">
-            <router-link class="nav-link" :to="{name: 'quanlymuonsach'}"><small>Xem thêm <i class="fas fa-arrow-circle-right"></i></small></router-link>
+            <router-link class="nav-link" :to="{ name: 'quanlymuonsach' }"><small>Xem thêm <i
+                  class="fas fa-arrow-circle-right"></i></small></router-link>
           </div>
         </div>
       </div>
       <div class="col-md-4">
-        <div class="card text-white text-center" :style="{backgroundColor: '#6f42c1' }">
+        <div class="card text-white text-center" :style="{ backgroundColor: '#6f42c1' }">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-center">
               <div>
@@ -65,13 +68,14 @@
             </div>
           </div>
           <div class="card-footer bg-transparent border-top-0">
-            <router-link class="nav-link" :to="{name: 'quanlymuonsach'}"><small>Xem thêm <i class="fas fa-arrow-circle-right"></i></small></router-link>
+            <router-link class="nav-link" :to="{ name: 'quanlymuonsach' }"><small>Xem thêm <i
+                  class="fas fa-arrow-circle-right"></i></small></router-link>
           </div>
         </div>
       </div>
 
       <div class="col-md-4">
-        <div class="card text-white text-center" :style="{backgroundColor: '#6f42c1' }">
+        <div class="card text-white text-center" :style="{ backgroundColor: '#6f42c1' }">
           <div class="card-body">
             <div class="d-flex justify-content-between align-items-center">
               <div>
@@ -82,7 +86,8 @@
             </div>
           </div>
           <div class="card-footer bg-transparent border-top-0">
-            <router-link class="nav-link" :to="{name: 'quanlymuonsach'}"><small>Xem thêm <i class="fas fa-arrow-circle-right"></i></small></router-link>
+            <router-link class="nav-link" :to="{ name: 'quanlydocgia' }"><small>Xem thêm <i
+                  class="fas fa-arrow-circle-right"></i></small></router-link>
           </div>
         </div>
       </div>
@@ -106,15 +111,15 @@ const readerCount = ref('')
 const bookCount = ref('')
 const borrowCount = ref('')
 const publisherCount = ref('')
-onMounted(async ()=> {
-    await bookStore.getAll()
-    await publisherStore.getAll()
-    await borrowStore.getAllForAdmin()
-    await readerStore.getAll()
-    readerCount.value = readerStore.count
-    bookCount.value = bookStore.count
-    publisherCount.value = publisherStore.count
-    borrowCount.value = borrowStore.count
+onMounted(async () => {
+  await bookStore.getAll()
+  await publisherStore.getAll()
+  await borrowStore.getAllForAdmin()
+  await readerStore.getAll()
+  readerCount.value = readerStore.count
+  bookCount.value = bookStore.count
+  publisherCount.value = publisherStore.count
+  borrowCount.value = borrowStore.count
 })
 </script>
 
@@ -123,18 +128,22 @@ onMounted(async ()=> {
   border-radius: 0.75rem;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
+
 .card-footer {
   color: white;
   cursor: pointer;
 }
+
 .card-footer:hover {
   text-decoration: underline;
 }
+
 .big-icon {
   font-size: 3rem;
 }
-.nav-link{
-    color: white;
-    text-decoration: none;
+
+.nav-link {
+  color: white;
+  text-decoration: none;
 }
 </style>
